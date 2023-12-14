0ef2e2e33b634424f20fa6c4473ea3556216ab7d
Jaythan <jaythan@JaythandeMacBook-Pro.local>
2023-12-14 18:39:31 +0800

    hightlight

--- lib/views/components/item_card.dart
+++ lib/views/components/item_card.dart
@@ -20,35 +20,48 @@ Widget itemCard(BuildContext context, item,
     child: GestureDetector(
       key: Key(item.id),
       onTap: item.status == Status.ACTIVED.name ? onTap : onTap,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 18.0 / 13.0,
-            child: Image.network(
-              item!.images.isEmpty
-                  ? ("$defaultImage?imageView2/1/w/268/q/85")
-                  : ("${item.images[0]}?imageView2/1/w/268/q/85"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16, right: 8, left: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(item.name,
-                      style: const TextStyle(
-                          color: Color(0xFF575E67), fontSize: 12.0)),
-                  Text("\$${(item.pricing / 100).toString()}",
-                      style: const TextStyle(
-                          color: Color(0xFFCC8053), fontSize: 12.0)),
-                ],
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 3.0,
+                  blurRadius: 5.0)
+            ],
+            color: item.status == Status.DEACTIVED.name
+                ? Color(0xFFFFECDF).withOpacity(0.5)
+                : Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18.0 / 13.0,
+              child: Image.network(
+                item!.images.isEmpty
+                    ? ("$defaultImage?imageView2/1/w/268/q/85")
+                    : ("${item.images[0]}?imageView2/1/w/268/q/85"),
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-        ],
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16, right: 8, left: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(item.name,
+                        style: const TextStyle(
+                            color: Color(0xFF575E67), fontSize: 12.0)),
+                    Text("\$${(item.pricing / 100).toString()}",
+                        style: const TextStyle(
+                            color: Color(0xFFCC8053), fontSize: 12.0)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   );
